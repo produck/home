f3165adfc5de04812468e047f4319a1326293b46
chaoslee <chaos2lee@qq.com>
2024-02-19 19:57:05 +0800

    [ADDED] vercel speed insights

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -17,5 +17,7 @@ export default {
 } satisfies Theme
 
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
 inject();
+injectSpeedInsights();
