ba2795bb73361d07ff9f983ef5d16af9a5f3dd8d
chaoslee <chaos2lee@qq.com>
2024-02-19 19:48:09 +0800

    [ADDED] vercel analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -5,13 +5,17 @@ import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
 export default {
-  extends: DefaultTheme,
-  Layout: () => {
-    return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp({ app, router, siteData }) {
-    // ...
-  }
+	extends: DefaultTheme,
+	Layout: () => {
+		return h(DefaultTheme.Layout, null, {
+			// https://vitepress.dev/guide/extending-default-theme#layout-slots
+		})
+	},
+	enhanceApp({ app, router, siteData }) {
+		// ...
+	}
 } satisfies Theme
+
+import { inject } from '@vercel/analytics';
+
+inject();
